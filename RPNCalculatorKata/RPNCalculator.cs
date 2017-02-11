afcaf87f412e8c8a6adb4a6a18fc9aa10f9118e1
JesusSalasMataro <jesus459@hotmail.es>
2017-02-11 20:15:45 +0100

    First implementation

---
+++ RPNCalculatorKata/RPNCalculator.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPNCalculatorKata
+{
+    public class RPNCalculator
+    {
+        public string Evaluate(string input)
+        {
+            string[] items = input.Split(' ');
+            int operationResult = 0;
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < items.Length; i++)
+            {
+                if (IsOperator(items[i]))
+                {
+                    operationResult = ProcessOperation(items[i], stack);
+                    stack.Push(operationResult);
+                }
+                else
+                {
+                    stack.Push(int.Parse(items[i]));
+                }                
+            }
+
+            return CalculateFinalResult(stack);
+        }
+
+        private int ProcessOperation(string item, Stack<int> stack)
+        {
+            int result = 0;
+            int secondOperand = stack.Pop();
+            int firstOperand = stack.Pop();            
+
+            if (item == "+")
+            {
+                result = firstOperand + secondOperand;
+            }
+            if (item == "-")
+            {
+                result = firstOperand - secondOperand;
+            }
+            if (item == "*")
+            {
+                result = firstOperand * secondOperand;
+            }
+            if (item == "/")
+            {
+                result = firstOperand / secondOperand;
+            }
+
+            return result;
+        }
+
+        private bool IsOperator(string item)
+        {
+            return item == "+" || item == "-" || item == "*" || item == "/";
+        }
+
+        private string CalculateFinalResult(Stack<int> stack)
+        {
+            string result = "";
+
+            while (stack.Count > 0)
+            {
+                result = stack.Pop().ToString() + " " + result;
+            }
+
+            return result.TrimEnd();
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RPNCalculatorKata;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void When_A_Digit_Is_Sent_Output_Digit()
+        {
+            // ARRANGE
+            string input = "1";
+            RPNCalculator calculator = new RPNCalculator();
+
+            // ACT
+            string actualResult = calculator.Evaluate(input);
+            string expectedResult = "1";
+
+            // ASSERT
+            Assert.IsTrue(expectedResult.Equals(actualResult));
+        }
+
+        [TestMethod]
+        public void When_Some_Digits_Are_Sent_Output_Number()
+        {
+            // ARRANGE
+            string input = "12 3";
+            RPNCalculator calculator = new RPNCalculator();
+
+            // ACT
+            string actualResult = calculator.Evaluate(input);
+            string expectedResult = "12 3";
+
+            // ASSERT
+            Assert.IsTrue(expectedResult.Equals(actualResult));
+        }
+
+        [TestMethod]
+        public void When_An_Operator_Is_Sent_After_Two_Numbers_Calculate_Result()
+        {
+            // ARRANGE
+            string input = "12 3 +";
+            RPNCalculator calculator = new RPNCalculator();
+
+            // ACT
+            string actualResult = calculator.Evaluate(input);
+            string expectedResult = "15";
+
+            // ASSERT
+            Assert.IsTrue(expectedResult.Equals(actualResult));
+        }
+
+        [TestMethod]
+        public void When_An_Operator_Is_Sent_After_Two_Numbers_Calculate_Result_Several_Operators_Case()
+        {
+            // ARRANGE
+            string input = "6 3 + 5 -";
+            RPNCalculator calculator = new RPNCalculator();
+
+            // ACT
+            string actualResult = calculator.Evaluate(input);
+            string expectedResult = "4";
+
+            // ASSERT
+            Assert.IsTrue(expectedResult.Equals(actualResult));
+        }
+
+        [TestMethod]
+        public void Many_Operands_And_Operators_Case()
+        {
+            // ARRANGE
+            string input = "3 5 8 * 7 + *";
+            RPNCalculator calculator = new RPNCalculator();
+
+            // ACT
+            string actualResult = calculator.Evaluate(input);
+            string expectedResult = "141";
+            
+            // ASSERT            
+            Assert.IsTrue(expectedResult.Equals(actualResult));
+        }
+
+        [TestMethod]
+        public void Operation_And_Two_Number_Combination_At_The_End_Case()
+        {
+            // ARRANGE
+            string input = "7 2 - 3 4";
+            RPNCalculator calculator = new RPNCalculator();
+
+            // ACT
+            string actualResult = calculator.Evaluate(input);
+            string expectedResult = "5 3 4";
+
+            // ASSERT
+            Assert.IsTrue(expectedResult.Equals(actualResult));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void Division_By_Zero_Exception_Case()
+        {
+            // ARRANGE
+            string input = "7 2 - 0 /";
+            RPNCalculator calculator = new RPNCalculator();
+
+            // ACT
+            string actualResult = calculator.Evaluate(input);
+
+            // ASSERT
+        }
+    }
+}
